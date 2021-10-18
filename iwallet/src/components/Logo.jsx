bb4e2bc6531e2a1c4c5572c4698e2d77922e0c2d
lgarceau <luke.garceau@ametek.com>
2021-10-18 17:05:06 -0400

    logo image with the withText prop

--- iwallet/src/components/Logo.jsx
+++ iwallet/src/components/Logo.jsx
@@ -2,10 +2,24 @@ import React, { useState } from 'react'
 import { StyleSheet } from 'react-native';
 import { SvgUri } from 'react-native-svg';
 
-const Logo = () => {
+const Logo = (props) => {
     const [loading, setLoading] = useState(false);
     const logoImage = "http://raw.githubusercontent.com/lgarceau768/iWallet/main/iwallet/assets/logo.svg";
-    const logoImage = "http://raw.githubusercontent.com/lgarceau768/iWallet/main/iwallet/assets/logo_small.svg";
+    const logoSmallImage = "http://raw.githubusercontent.com/lgarceau768/iWallet/main/iwallet/assets/logo_small.svg";
+    const isSmall = props.withText ?? false; // true false
+    if(isSmall) {
+        return (
+            <SvgUri 
+                width="100%"
+                height="100%"
+                uri={logoSmallImage}
+                style={styles.logo}
+                onLoadStart={() => setLoading(true)}
+                onLoadEnd={() => setLoading(false)}
+            />
+        )
+    }
+    // render for the logo with the image
     return (
         <SvgUri 
             width="100%"
